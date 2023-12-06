2a9a55e18905e89be0a7a44fb0f43954b4899807
tal <869tal@gmail.com>
2023-12-06 14:48:00 +0200

    update pc store, added assembly code, added pacman

---
+++ assembly/name and numbers.asm
@@ -0,0 +1,44 @@
+mov ah,2
+mov dl, 'T'
+int 21h
+mov dl, 'A'
+int 21h     
+mov dl, 'L'
+int 21h     
+mov dl, 10
+int 21h
+mov dl, 13
+int 21h
+mov dl, '0'
+int 21h      
+mov dl, ' '
+int 21h
+mov dl, '1'
+int 21h
+mov dl, ' '
+int 21h
+mov dl, '2'  
+int 21h
+mov dl, ' '
+int 21h
+mov dl, '3'
+int 21h
+mov dl, ' '
+int 21h
+mov dl, '4'
+int 21h
+mov dl, ' '
+int 21h   
+mov dl, '5'
+int 21h
+mov dl, 10
+int 21h
+mov dl, 13
+int 21h
+mov dl, 't'
+int 21h
+mov dl, 'a'
+int 21h     
+mov dl, 'l'
+int 21h     
+
